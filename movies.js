99c57cc2cb87213e7116830e0e2eb1c647abc1b6
anhdong04 <anhdong040903@gmail.com>
2025-12-09 11:34:45 +0700

    Add new movie 'Mang Mẹ Đi Bỏ' to movieList
    Added new movie 'Mang Mẹ Đi Bỏ' to the movie list.

--- movies.js
+++ movies.js
@@ -10,4 +10,12 @@ const movieList = [
         "release_date": "2024",
         "dropbox_link": "#"
     },
+    {
+    "title": "Mang Mẹ Đi Bỏ",
+    "original_title": "Mang Mẹ Đi Bỏ",
+    "overview": "Rời Xa Mẹ kể về câu chuyện của Hoan (Tuấn Trần), một chàng trai trẻ kiếm sống bằng nghề cắt tóc 'gã hề đường phố' để chăm sóc mẹ. Mẹ anh, Lê Thị Hạnh (Hồng Đào), mắc bệnh Alzheimer và cư xử như một đứa trẻ. Căn bệnh của bà không chỉ là gánh nặng tài chính mà còn tước đi cuộc sống tự do và những ước mơ mà anh vẫn đang theo đuổi. Một ngày nọ, Hoan đưa ra một quyết định định mệnh - 'gửi mẹ' cho anh trai mình ở Hàn Quốc, một người mà anh thậm chí chưa từng gặp mặt.",
+    "poster_path": "https://image.tmdb.org/t/p/w500/ene2eO4C7We80YIcu7yX9zVGsSS.jpg",
+    "release_date": "2025-07-30",
+    "dropbox_link": "https://www.dropbox.com/scl/fi/z4y1rojm0zztpvswfw8wd/Leaving-Mom.2025.1080p.WEB-DL.H264.AAC.mp4?rlkey=6gi8u981a64eq8eypyg5fvs2b&st=j7due4jf&dl=1?dl=1"
+},
 ];
