cb6d8231b1e4f186254a253bf256e93ceb055e9d
anhdong04 <anhdong040903@gmail.com>
2025-12-09 11:32:43 +0700

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,13 @@
+// movies.js
+const movieList = [
+    // Bạn sẽ dán code phim mới vào dưới dòng này
+    {
+        "id": 1,
+        "title": "Example Movie (Mẫu)",
+        "original_title": "Phim Mẫu",
+        "overview": "Mô tả phim mẫu...",
+        "poster_path": "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+        "release_date": "2024",
+        "dropbox_link": "#"
+    },
+];
