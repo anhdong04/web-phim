645c47fbe9ffefb5eb33e2df069a4db6a57b8fc5
anhdong04 <anhdong040903@gmail.com>
2025-12-09 11:46:40 +0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -18,4 +18,24 @@ const movieList = [
         // SỬA DÒNG DƯỚI ĐÂY (xóa ?dl=1 thừa ở cuối)
         "dropbox_link": "https://www.dropbox.com/scl/fi/z4y1rojm0zztpvswfw8wd/Leaving-Mom.2025.1080p.WEB-DL.H264.AAC.mp4?rlkey=6gi8u981a64eq8eypyg5fvs2b&st=j7due4jf&dl=1"
     }
+    {
+    "id": 1010756,
+    "title": "Sát Nhân Giấu Mặt: Chương 2",
+    "original_title": "The Strangers: Chapter 2",
+    "overview": "Sau những sự kiện kinh hoàng ở phần trước, Maya bị mắc kẹt tại một thị trấn nhỏ không lối thoát. Khi màn đêm buông xuống, ba kẻ sát nhân đeo mặt nạ lại xuất hiện, biến nơi đây thành địa ngục trần gian. Maya không chỉ phải chạy trốn mà còn phải đối mặt với quá khứ và nỗi sợ hãi sâu thẳm nhất của mình.",
+    "poster_path": "https://image.tmdb.org/t/p/w500/aEk9jLbiKTVssdATbrF879NvyyJ.jpg",
+    "release_date": "2025-09-25",
+    "vote_average": 6.11,
+    "dropbox_link": "https://www.dropbox.com/scl/fi/mjw76u8iknlaviooh7t61/The.Strangers.Chapter.2.2025.Hybrid.1080p.BluRay.x264-BRUTE.mkv?rlkey=o7lbomu84wex3zmmtkpoulisi&st=jqhmtvsd&dl=0&dl=1"
+},
+
+
+
+
+
+
+
+
+    
 ];
+
