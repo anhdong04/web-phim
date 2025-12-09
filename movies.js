ff3b46916a08f1bcf5dcd2a75c91421b3d18985b
anhdong04 <anhdong040903@gmail.com>
2025-12-09 11:50:14 +0700

    Revise movieList with new entries and details
    Updated movie details for 'Mang Mẹ Đi Bỏ' and adjusted links.

--- movies.js
+++ movies.js
@@ -1,22 +1,14 @@
 // movies.js
 const movieList = [
-    {
-        "id": 1,
-        "title": "Example Movie (Mẫu)",
-        "original_title": "Phim Mẫu",
-        "overview": "Mô tả phim mẫu...",
-        "poster_path": "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
-        "release_date": "2024",
-        "dropbox_link": "#"
-    },
-    {
-        "title": "Mang Mẹ Đi Bỏ",
-        "original_title": "Mang Mẹ Đi Bỏ",
-        "overview": "Rời Xa Mẹ kể về câu chuyện của Hoan...",
-        "poster_path": "https://image.tmdb.org/t/p/w500/ene2eO4C7We80YIcu7yX9zVGsSS.jpg",
-        "release_date": "2025-07-30",
-        // SỬA DÒNG DƯỚI ĐÂY (xóa ?dl=1 thừa ở cuối)
-        "dropbox_link": "https://www.dropbox.com/scl/fi/z4y1rojm0zztpvswfw8wd/Leaving-Mom.2025.1080p.WEB-DL.H264.AAC.mp4?rlkey=6gi8u981a64eq8eypyg5fvs2b&st=j7due4jf&dl=1"
+   {
+    "id": 1478821,
+    "title": "Mang Mẹ Đi Bỏ",
+    "original_title": "Mang Mẹ Đi Bỏ",
+    "overview": "Rời Xa Mẹ kể về câu chuyện của Hoan (Tuấn Trần), một chàng trai trẻ kiếm sống bằng nghề cắt tóc 'gã hề đường phố' để chăm sóc mẹ. Mẹ anh, Lê Thị Hạnh (Hồng Đào), mắc bệnh Alzheimer và cư xử như một đứa trẻ. Căn bệnh của bà không chỉ là gánh nặng tài chính mà còn tước đi cuộc sống tự do và những ước mơ mà anh vẫn đang theo đuổi. Một ngày nọ, Hoan đưa ra một quyết định định mệnh - 'gửi mẹ' cho anh trai mình ở Hàn Quốc, một người mà anh thậm chí chưa từng gặp mặt.",
+    "poster_path": "https://image.tmdb.org/t/p/w500/ene2eO4C7We80YIcu7yX9zVGsSS.jpg",
+    "release_date": "2025-07-30",
+    "vote_average": 8,
+    "dropbox_link": "https://www.dropbox.com/scl/fi/z4y1rojm0zztpvswfw8wd/Leaving-Mom.2025.1080p.WEB-DL.H264.AAC.mp4?rlkey=6gi8u981a64eq8eypyg5fvs2b&st=j7due4jf&dl=1&dl=1"
     },
     {
     "id": 1010756,
@@ -38,5 +30,6 @@ const movieList = [
 
     
 ];
+
 
 
