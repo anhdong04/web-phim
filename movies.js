4d0cb69c76866916e21498efb835ebf5ae102609
anhdong04 <anhdong040903@gmail.com>
2025-12-09 11:37:31 +0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,6 +1,5 @@
 // movies.js
 const movieList = [
-    // Bạn sẽ dán code phim mới vào dưới dòng này
     {
         "id": 1,
         "title": "Example Movie (Mẫu)",
@@ -11,11 +10,12 @@ const movieList = [
         "dropbox_link": "#"
     },
     {
-    "title": "Mang Mẹ Đi Bỏ",
-    "original_title": "Mang Mẹ Đi Bỏ",
-    "overview": "Rời Xa Mẹ kể về câu chuyện của Hoan (Tuấn Trần), một chàng trai trẻ kiếm sống bằng nghề cắt tóc 'gã hề đường phố' để chăm sóc mẹ. Mẹ anh, Lê Thị Hạnh (Hồng Đào), mắc bệnh Alzheimer và cư xử như một đứa trẻ. Căn bệnh của bà không chỉ là gánh nặng tài chính mà còn tước đi cuộc sống tự do và những ước mơ mà anh vẫn đang theo đuổi. Một ngày nọ, Hoan đưa ra một quyết định định mệnh - 'gửi mẹ' cho anh trai mình ở Hàn Quốc, một người mà anh thậm chí chưa từng gặp mặt.",
-    "poster_path": "https://image.tmdb.org/t/p/w500/ene2eO4C7We80YIcu7yX9zVGsSS.jpg",
-    "release_date": "2025-07-30",
-    "dropbox_link": "https://www.dropbox.com/scl/fi/z4y1rojm0zztpvswfw8wd/Leaving-Mom.2025.1080p.WEB-DL.H264.AAC.mp4?rlkey=6gi8u981a64eq8eypyg5fvs2b&st=j7due4jf&dl=1?dl=1"
-},
+        "title": "Mang Mẹ Đi Bỏ",
+        "original_title": "Mang Mẹ Đi Bỏ",
+        "overview": "Rời Xa Mẹ kể về câu chuyện của Hoan...",
+        "poster_path": "https://image.tmdb.org/t/p/w500/ene2eO4C7We80YIcu7yX9zVGsSS.jpg",
+        "release_date": "2025-07-30",
+        // SỬA DÒNG DƯỚI ĐÂY (xóa ?dl=1 thừa ở cuối)
+        "dropbox_link": "https://www.dropbox.com/scl/fi/z4y1rojm0zztpvswfw8wd/Leaving-Mom.2025.1080p.WEB-DL.H264.AAC.mp4?rlkey=6gi8u981a64eq8eypyg5fvs2b&st=j7due4jf&dl=1"
+    }
 ];
