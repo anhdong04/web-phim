e7241811c5f4cef3cfd1e0eb0d694ee956daa657
anhdong04 <anhdong040903@gmail.com>
2026-08-14 22:48:33 +0700

    Fix generated HTTPS validation in v6 patch

--- v600_patch.js
+++ v600_patch.js
@@ -1,1 +1,6 @@
-module.exports = require('./v600_patch_impl');
+const applyV600Impl = require('./v600_patch_impl');
+
+module.exports = function applyV600(source) {
+  const out = applyV600Impl(source);
+  return out.replace("!/^https:///i.test(String(p?.u || ''))", "!String(p?.u || '').startsWith('https://')");
+};
