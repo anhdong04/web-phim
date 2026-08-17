f9a9e991efff7bff9bf284712ca10c9bc4bfc337
anhdong04 <anhdong040903@gmail.com>
2026-08-17 16:08:46 +0700

    Remove obsolete HH4K browser challenge patch

--- addon_v650.js
+++ addon_v650.js
@@ -2,6 +2,5 @@
 
 // HH4K interceptor is installed first, then the existing v6.4.1 launcher keeps
 // handling every non-HH4K route exactly as before.
-require('./hh4k_browser_patch');
 require('./hh4k_bridge');
 require('./addon_v641');
